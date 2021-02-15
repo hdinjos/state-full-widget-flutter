58068ad0c990dea8d138b3621b63ecb4bf01eab1
hdinjos <hdinjos@gmail.com>
2021-02-15 15:33:07 +0700

    Container &  BorderRadius

--- lib/main.dart
+++ lib/main.dart
@@ -9,21 +9,40 @@ class MyApp extends StatelessWidget {
   build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(
-            title: Text("Judul AppBar"),
-          ),
-          body: Center(
-            child: Container(
-              width: 300,
-              height: 400,
-              alignment: Alignment.center,
-              color: Color(0xffdc143c),
-              child: Text(
-                "Selamat datang bosq",
-                style: TextStyle(fontSize: 20, color: Colors.white),
-              ),
+        appBar: AppBar(
+          title: Text("Judul AppBar"),
+        ),
+        body: Container(
+          decoration: BoxDecoration(
+            color: Colors.lightGreen,
+            image: DecorationImage(
+              image: NetworkImage(
+                  "https://i.pinimg.com/originals/91/86/6b/91866b918c9cca0747f483a46943e926.jpg"),
+              fit: BoxFit.cover,
             ),
-          )),
+            border: Border.all(
+              color: Colors.black,
+              width: 5,
+            ),
+            borderRadius: BorderRadius.circular(20),
+          ),
+          width: 300,
+          height: 300,
+          transform: Matrix4.rotationZ(0.08),
+          margin: EdgeInsets.only(
+            top: 30,
+            bottom: 30,
+            left: 30,
+            right: 30,
+          ),
+          alignment: Alignment.center,
+          // color: Color(0xffdc143c),
+          child: Text(
+            "Selamat datang bosq",
+            style: TextStyle(fontSize: 20, color: Colors.white),
+          ),
+        ),
+      ),
     );
   }
 }
