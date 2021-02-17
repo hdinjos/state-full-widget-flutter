c84c86c6473bcc065ccc2f475621e2e86eb81503
hdinjos <hdinjos@gmail.com>
2021-02-17 15:03:40 +0700

    kombinasi gridview

--- lib/main.dart
+++ lib/main.dart
@@ -1,61 +1,55 @@
 import "package:flutter/material.dart";
+import 'package:flutter/rendering.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  final List pages = [1, 2, 3, 4, 5, 6, 7, 9, 10, 11, 12, 13];
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("Grid View"),
-        ),
-        body: GridView.count(
-          crossAxisCount: 2,
-          padding: EdgeInsets.all(5),
-          children: [
-            Container(
-              color: Colors.purple,
-              padding: EdgeInsets.all(20),
-              margin: EdgeInsets.all(5),
-            ),
-            Container(
-              color: Colors.purple,
-              margin: EdgeInsets.all(5),
-            ),
-            Container(
-              color: Colors.purple,
-              margin: EdgeInsets.all(5),
-            ),
-            Container(
-              color: Colors.purple,
-              margin: EdgeInsets.all(5),
-            ),
-            Container(
-              color: Colors.purple,
-              margin: EdgeInsets.all(5),
-            ),
-            Container(
-              color: Colors.purple,
-              margin: EdgeInsets.all(5),
-            ),
-            Container(
-              color: Colors.purple,
-              margin: EdgeInsets.all(5),
-            ),
-            Container(
-              color: Colors.purple,
-              margin: EdgeInsets.all(5),
-            ),
-            Container(
-              color: Colors.purple,
-              margin: EdgeInsets.all(5),
-            ),
-          ],
-        ),
-      ),
+          appBar: AppBar(
+            title: Text("Grid View"),
+          ),
+          body: GridView(
+            gridDelegate:
+                SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 1),
+            children: [
+              GridView.count(
+                // childAspectRatio: 3 / 4,
+                scrollDirection: Axis.horizontal,
+                crossAxisCount: 3,
+                // reverse: true,
+                padding: EdgeInsets.all(10),
+                children: List.generate(
+                  pages.length,
+                  (index) {
+                    return Container(
+                      child: Card(
+                        color: Colors.purpleAccent,
+                        child: Container(
+                          alignment: Alignment.center,
+                          child: Text(
+                            "Pages " + pages[index].toString(),
+                            style: TextStyle(
+                              fontSize: 18,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+              FlutterLogo(),
+              FlutterLogo(),
+              FlutterLogo(),
+            ],
+          )),
     );
   }
 }
