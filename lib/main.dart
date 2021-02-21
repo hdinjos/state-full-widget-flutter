6f00029b9132526ffe33d74ace2d463ebb2fba8f
hdinjos <hdinjos@gmail.com>
2021-02-21 22:20:26 +0700

    appbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,72 +1,59 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  Future<Album> futureAlbum;
-  Future<Album> fetchAlbum() async {
-    final response =
-        await http.get('https://jsonplaceholder.typicode.com/albums/1');
-
-    if (response.statusCode == 200) {
-      return Album.fromJson(jsonDecode(response.body));
-    } else {
-      throw Exception('failed to load album');
-    }
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    futureAlbum = fetchAlbum();
-  }
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Request api Get"),
-        ),
-        body: Center(
-          child: FutureBuilder<Album>(
-            future: futureAlbum,
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                return Text(snapshot.data.title);
-              } else if (snapshot.hasError) {
-                return Text("${snapshot.error}");
-              }
-              return CircularProgressIndicator();
-            },
+          toolbarOpacity: 0.5,
+          elevation: 0,
+          // centerTitle: true,
+          brightness: Brightness.dark,
+          titleSpacing: -2,
+          // backgroundColor: Colors.deepPurple,
+          flexibleSpace: Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  begin: Alignment.bottomLeft,
+                  end: Alignment.bottomRight,
+                  colors: [
+                    Colors.yellow,
+                    Colors.pink,
+                    Colors.cyan,
+                  ]),
+            ),
           ),
+          // title: Image.network(
+          //     'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTybCniORbVSJJgvqcMaMwxSY4IL6OHxKftgQ&usqp=CAU'),
+          title: Text("Selamat Malam"),
+          leading: Icon(Icons.home),
+          actions: [
+            IconButton(
+              onPressed: () => {
+                print("Halo dunia"),
+              },
+              icon: Icon(
+                Icons.call,
+                color: Colors.white,
+              ),
+            ),
+            IconButton(
+              onPressed: () => {
+                print("Halo world"),
+              },
+              icon: Icon(
+                Icons.search,
+                color: Colors.white,
+              ),
+            )
+          ],
         ),
       ),
-    );
-  }
-}
-
-class Album {
-  final int userId;
-  final int id;
-  final String title;
-
-  Album({this.userId, this.id, this.title});
-
-  factory Album.fromJson(Map<String, dynamic> json) {
-    return Album(
-      userId: json['userId'],
-      id: json['id'],
-      title: json['title'],
     );
   }
 }
